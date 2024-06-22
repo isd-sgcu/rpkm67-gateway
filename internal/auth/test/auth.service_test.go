46eb96ab6c26d6e65fad6997a6baf4ab41011c07
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-23 00:50:29 +0700

    fix: lint

--- internal/auth/test/auth.service_test.go
+++ internal/auth/test/auth.service_test.go
@@ -3,15 +3,13 @@ package test
 import (
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
-	"go.uber.org/zap"
 )
 
 type AuthServiceTest struct {
 	suite.Suite
-	controller *gomock.Controller
-	logger     *zap.Logger
+	// controller *gomock.Controller
+	// logger     *zap.Logger
 }
 
 func TestAuthService(t *testing.T) {
