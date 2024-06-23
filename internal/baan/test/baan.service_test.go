c444d287a2f498c3367f972d0b7c124edfc5d1a9
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-23 15:33:38 +0700

    fix: sfvc test

--- internal/baan/test/baan.service_test.go
+++ internal/baan/test/baan.service_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/isd-sgcu/rpkm67-gateway/internal/dto"
 	baanMock "github.com/isd-sgcu/rpkm67-gateway/mocks/client/baan"
 	baanProto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/backend/baan/v1"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
 )
@@ -53,43 +52,40 @@ func (t *BaanServiceTest) SetupTest() {
 }
 
 func (t *BaanServiceTest) TestFindAllBaanSuccess() {
+	client := baanMock.BaanClientMock{}
+	svc := baan.NewService(&client, t.logger)
+
 	protoResp := &baanProto.FindAllBaanResponse{
 		Baans: t.BaansProto,
 	}
-
 	findAllBaansDto := baan.ProtoToDtoList(protoResp.Baans)
-
 	expected := &dto.FindAllBaanResponse{
 		Baans: findAllBaansDto,
 	}
 
-	client := baanMock.BaanClientMock{}
 	client.On("FindAllBaan", t.FindAllBaanProtoReq).Return(protoResp, nil)
-
-	svc := baan.NewService(&client, t.logger)
 	actual, err := svc.FindAllBaan(t.FindAllBaanDtoReq)
 
-	assert.Nil(t.T(), err)
-	assert.Equal(t.T(), expected, actual)
+	t.Nil(err)
+	t.Equal(expected, actual)
 }
 
 func (t *BaanServiceTest) TestFindOneBaanSuccess() {
+	client := baanMock.BaanClientMock{}
+	svc := baan.NewService(&client, t.logger)
+
 	protoResp := &baanProto.FindOneBaanResponse{
 		Baan: t.BaanProto,
 	}
-
 	expected := &dto.FindOneBaanResponse{
 		Baan: t.BaanDto,
 	}
 
-	client := baanMock.BaanClientMock{}
 	client.On("FindOneBaan", t.FindOneBaanProtoReq).Return(protoResp, nil)
-
-	svc := baan.NewService(&client, t.logger)
 	actual, err := svc.FindOneBaan(t.FindOneBaanDtoReq)
 
-	assert.Nil(t.T(), err)
-	assert.Equal(t.T(), expected, actual)
+	t.Nil(err)
+	t.Equal(expected, actual)
 }
 
 func (t *BaanServiceTest) TearDownTest() {
