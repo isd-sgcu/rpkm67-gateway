d9ea2e7f070d9e2bfcd987f6436c042eb35a719e
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-24 23:32:40 +0700

    feat: setup auth gg

--- internal/auth/auth.handler.go
+++ internal/auth/auth.handler.go
@@ -1,9 +1,6 @@
 package auth
 
 import (
-	"strings"
-
-	"github.com/isd-sgcu/rpkm67-gateway/internal/dto"
 	"github.com/isd-sgcu/rpkm67-gateway/internal/router"
 	"github.com/isd-sgcu/rpkm67-gateway/internal/validator"
 	"go.uber.org/zap"
@@ -12,11 +9,8 @@ import (
 type Handler interface {
 	Validate(c router.Context)
 	RefreshToken(c router.Context)
-	SignUp(c router.Context)
-	SignIn(c router.Context)
-	SignOut(c router.Context)
-	ForgotPassword(c router.Context)
-	ResetPassword(c router.Context)
+	GetGoogleLoginUrl(c router.Context)
+	VerifyGoogleLogin(c router.Context)
 }
 
 type handlerImpl struct {
@@ -39,44 +33,38 @@ func (h *handlerImpl) Validate(c router.Context) {
 func (h *handlerImpl) RefreshToken(c router.Context) {
 }
 
-func (h *handlerImpl) SignUp(c router.Context) {
-	body := &dto.SignUpRequest{}
-	if err := c.Bind(body); err != nil {
-		h.log.Named("auth hdr").Error("failed to bind request body", zap.Error(err))
-		c.BadRequestError(err.Error())
-		return
-	}
-
-	if errorList := h.validate.Validate(body); errorList != nil {
-		h.log.Named("auth hdr").Error("validation error", zap.Strings("errorList", errorList))
-		c.BadRequestError(strings.Join(errorList, ", "))
-		return
-	}
-
-	req := &dto.SignUpRequest{
-		Email:     body.Email,
-		Password:  body.Password,
-		Firstname: body.Firstname,
-		Lastname:  body.Lastname,
-	}
-
-	credential, appErr := h.svc.SignUp(req)
-	if appErr != nil {
-		c.ResponseError(appErr)
-		return
-	}
-
-	c.JSON(201, credential)
+func (h *handlerImpl) GetGoogleLoginUrl(c router.Context) {
 }
 
-func (h *handlerImpl) SignIn(c router.Context) {
+func (h *handlerImpl) VerifyGoogleLogin(c router.Context) {
 }
 
-func (h *handlerImpl) SignOut(c router.Context) {
-}
+// func (h *handlerImpl) SignUp(c router.Context) {
+// 	body := &dto.SignUpRequest{}
+// 	if err := c.Bind(body); err != nil {
+// 		h.log.Named("auth hdr").Error("failed to bind request body", zap.Error(err))
+// 		c.BadRequestError(err.Error())
+// 		return
+// 	}
 
-func (h *handlerImpl) ForgotPassword(c router.Context) {
-}
+// 	if errorList := h.validate.Validate(body); errorList != nil {
+// 		h.log.Named("auth hdr").Error("validation error", zap.Strings("errorList", errorList))
+// 		c.BadRequestError(strings.Join(errorList, ", "))
+// 		return
+// 	}
 
-func (h *handlerImpl) ResetPassword(c router.Context) {
-}
+// 	req := &dto.SignUpRequest{
+// 		Email:     body.Email,
+// 		Password:  body.Password,
+// 		Firstname: body.Firstname,
+// 		Lastname:  body.Lastname,
+// 	}
+
+// 	credential, appErr := h.svc.SignUp(req)
+// 	if appErr != nil {
+// 		c.ResponseError(appErr)
+// 		return
+// 	}
+
+// 	c.JSON(201, credential)
+// }
--- internal/auth/auth.service.go
+++ internal/auth/auth.service.go
@@ -1,25 +1,17 @@
 package auth
 
 import (
-	"context"
-	"time"
-
 	"github.com/isd-sgcu/rpkm67-gateway/apperror"
 	"github.com/isd-sgcu/rpkm67-gateway/internal/dto"
 	authProto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/auth/auth/v1"
 	"go.uber.org/zap"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type Service interface {
 	Validate()
 	RefreshToken()
-	SignUp(req *dto.SignUpRequest) (*dto.SignupResponse, *apperror.AppError)
-	SignIn(req *dto.SignInRequest) (*dto.Credential, *apperror.AppError)
-	SignOut(req *dto.TokenPayloadAuth) (*dto.SignOutResponse, *apperror.AppError)
-	ForgotPassword(req *dto.ForgotPasswordRequest) (*dto.ForgotPasswordResponse, *apperror.AppError)
-	ResetPassword(req *dto.ResetPasswordRequest) (*dto.ResetPasswordResponse, *apperror.AppError)
+	GetGoogleLoginUrl()
+	VerifyGoogleLogin()
 }
 
 type serviceImpl struct {
@@ -39,77 +31,75 @@ func (s *serviceImpl) Validate() {
 func (s *serviceImpl) RefreshToken() {
 }
 
+func (s *serviceImpl) GetGoogleLoginUrl() {
+}
+
+func (s *serviceImpl) VerifyGoogleLogin() {
+}
+
 func (s *serviceImpl) SignUp(req *dto.SignUpRequest) (*dto.SignupResponse, *apperror.AppError) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
 
-	res, err := s.client.SignUp(ctx, &authProto.SignUpRequest{
-		Email:     req.Email,
-		Password:  req.Password,
-		Firstname: req.Firstname,
-		Lastname:  req.Lastname,
-	})
-	if err != nil {
-		st, ok := status.FromError(err)
-		if !ok {
-			return nil, apperror.InternalServer
-		}
-		switch st.Code() {
-		case codes.AlreadyExists:
-			return nil, apperror.BadRequestError("User already exists")
-		case codes.Internal:
-			return nil, apperror.InternalServerError(err.Error())
-		default:
-			return nil, apperror.ServiceUnavailable
-		}
-	}
+	// res, err := s.client.SignUp(ctx, &authProto.SignUpRequest{
+	// 	Email:     req.Email,
+	// 	Password:  req.Password,
+	// 	Firstname: req.Firstname,
+	// 	Lastname:  req.Lastname,
+	// })
+	// if err != nil {
+	// 	st, ok := status.FromError(err)
+	// 	if !ok {
+	// 		return nil, apperror.InternalServer
+	// 	}
+	// 	switch st.Code() {
+	// 	case codes.AlreadyExists:
+	// 		return nil, apperror.BadRequestError("User already exists")
+	// 	case codes.Internal:
+	// 		return nil, apperror.InternalServerError(err.Error())
+	// 	default:
+	// 		return nil, apperror.ServiceUnavailable
+	// 	}
+	// }
 
-	return &dto.SignupResponse{
-		Id:        res.Id,
-		Email:     res.Email,
-		Firstname: res.Firstname,
-		Lastname:  res.Lastname,
-	}, nil
+	// return &dto.SignupResponse{
+	// 	Id:        res.Id,
+	// 	Email:     res.Email,
+	// 	Firstname: res.Firstname,
+	// 	Lastname:  res.Lastname,
+	// }, nil
+
+	return nil, nil
 }
 
 func (s *serviceImpl) SignIn(req *dto.SignInRequest) (*dto.Credential, *apperror.AppError) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
 
-	res, err := s.client.SignIn(ctx, &authProto.SignInRequest{
-		Email:    req.Email,
-		Password: req.Password,
-	})
-	if err != nil {
-		st, ok := status.FromError(err)
-		if !ok {
-			return nil, apperror.InternalServer
-		}
-		switch st.Code() {
-		case codes.AlreadyExists:
-			return nil, apperror.BadRequestError("User already exists")
-		case codes.Internal:
-			return nil, apperror.InternalServerError(err.Error())
-		default:
-			return nil, apperror.ServiceUnavailable
-		}
-	}
+	// res, err := s.client.SignIn(ctx, &authProto.SignInRequest{
+	// 	Email:    req.Email,
+	// 	Password: req.Password,
+	// })
+	// if err != nil {
+	// 	st, ok := status.FromError(err)
+	// 	if !ok {
+	// 		return nil, apperror.InternalServer
+	// 	}
+	// 	switch st.Code() {
+	// 	case codes.AlreadyExists:
+	// 		return nil, apperror.BadRequestError("User already exists")
+	// 	case codes.Internal:
+	// 		return nil, apperror.InternalServerError(err.Error())
+	// 	default:
+	// 		return nil, apperror.ServiceUnavailable
+	// 	}
+	// }
 
-	return &dto.Credential{
-		AccessToken:  res.Credential.AccessToken,
-		RefreshToken: res.Credential.RefreshToken,
-		ExpiresIn:    int(res.Credential.ExpiresIn),
-	}, nil
-}
+	// return &dto.Credential{
+	// 	AccessToken:  res.Credential.AccessToken,
+	// 	RefreshToken: res.Credential.RefreshToken,
+	// 	ExpiresIn:    int(res.Credential.ExpiresIn),
+	// }, nil
 
-func (s *serviceImpl) SignOut(req *dto.TokenPayloadAuth) (*dto.SignOutResponse, *apperror.AppError) {
-	return nil, nil
-}
-
-func (s *serviceImpl) ForgotPassword(req *dto.ForgotPasswordRequest) (*dto.ForgotPasswordResponse, *apperror.AppError) {
-	return nil, nil
-}
-
-func (s *serviceImpl) ResetPassword(req *dto.ResetPasswordRequest) (*dto.ResetPasswordResponse, *apperror.AppError) {
 	return nil, nil
 }
