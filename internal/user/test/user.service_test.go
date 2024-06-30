a6aeb9882e70891ac0b9351aaf4f4a922658a434
Wasawat Junnasaksri <ice7533678@gmail.com>
2024-06-30 22:00:11 +0700

    feat: add user_handler test

--- internal/user/test/user.service_test.go
+++ internal/user/test/user.service_test.go
@@ -4,6 +4,13 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/isd-sgcu/rpkm67-gateway/apperror"
+	"github.com/isd-sgcu/rpkm67-gateway/internal/dto"
+	"github.com/isd-sgcu/rpkm67-gateway/internal/user"
+	"github.com/isd-sgcu/rpkm67-gateway/internal/group"
+	userMock "github.com/isd-sgcu/rpkm67-gateway/mocks/user"
+	userProto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/auth/user/v1"
+	objectMock "github.com/isd-sgcu/rpkm67-gateway/mocks/object"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/zap"
 )
@@ -12,6 +19,22 @@ type UserServiceTest struct {
 	suite.Suite
 	controller *gomock.Controller
 	logger     *zap.Logger
+
+	userProto *userProto.User
+	userDto *dto.User
+
+	Groups                   []*dto.Group
+	Group                    *dto.Group
+
+	FindByEmailRequestProto *userProto.FindByEmailRequest
+	
+	FindOneUserRequestProto *userProto.FindOneUserRequest
+	FindOneUserRequestDto *dto.FindOneUserRequest
+	UpdateUserProfileRequestProto *userProto.UpdateUserRequest
+	UpdateUserProfileRequestDto *dto.UpdateUserProfileRequest
+	UpdateUserPictureRequestDto *dto.UpdateUserPictureRequest
+	ObjectService *dto.Object
+	Err apperror.AppError
 }
 
 func TestUserService(t *testing.T) {
@@ -21,10 +44,44 @@ func TestUserService(t *testing.T) {
 func (t *UserServiceTest) SetupTest() {
 	t.controller = gomock.NewController(t.T())
 	t.logger = zap.NewNop()
+
+	groupsProto := MockGroupsProto()
+	groupProto := groupsProto[0]
+
+	t.Groups = group.GroupProtoToDtoList(groupsProto)
+	t.Group = group.GroupProtoToDto(groupProto)
+
+	t.userProto = MockUserProto()
+	t.userDto = user.ProtoToDto(t.userProto)
+
+	t.FindOneUserRequestProto = &userProto.FindOneUserRequest{
+		Id: t.userProto.Id,
+	}
+
+	t.FindOneUserRequestDto = &dto.FindOneUserRequest{
+		Id: t.userDto.Id,
+	}
 }
 
 func (t *UserServiceTest) TestFindOneUserSuccess() {
+	client := userMock.NewMockClient(t.controller)
+	objSvc := objectMock.NewMockService(t.controller)
+	svc := user.NewService(client, objSvc, t.logger)
+	
+	protoResp := &userProto.FindOneUserResponse{
+		User: t.userProto,
+	}
 
+	createUserDto := user.ProtoToDto(protoResp.User)
+	expected := &dto.FindOneUserResponse{
+		User: createUserDto,
+	}
+
+	client.EXPECT().Create(gomock.Any(), t.FindOneUserRequestProto).Return(protoResp, nil)
+	actual, err := svc.FindOne(t.FindOneUserRequestDto)
+
+	t.Nil(err)
+	t.Equal(expected, actual)
 }
 
 func (t *UserServiceTest) TearDownTest() {
