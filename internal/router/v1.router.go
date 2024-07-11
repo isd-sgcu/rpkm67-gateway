1947eb96ad314c243ce37106b79d0c3b836de90f
Gear <84141000+macgeargear@users.noreply.github.com>
2024-07-11 16:18:44 +0700

    feat: inject tracer to router

--- internal/router/v1.router.go
+++ internal/router/v1.router.go
@@ -8,30 +8,30 @@ import (
 
 func (r *Router) V1Get(path string, handler func(c context.Ctx)) {
 	r.V1.GET(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.GET, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.GET, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1Post(path string, handler func(c context.Ctx)) {
 	r.V1.POST(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.POST, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.POST, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1Put(path string, handler func(c context.Ctx)) {
 	r.V1.PUT(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.PUT, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.PUT, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1Patch(path string, handler func(c context.Ctx)) {
 	r.V1.PATCH(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.PATCH, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.PATCH, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1Delete(path string, handler func(c context.Ctx)) {
 	r.V1.DELETE(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.DELETE, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.DELETE, path, r.requestMetrics, r.tracer))
 	})
 }
--- internal/router/v1NonAuth.router.go
+++ internal/router/v1NonAuth.router.go
@@ -8,30 +8,30 @@ import (
 
 func (r *Router) V1NonAuthGet(path string, handler func(c context.Ctx)) {
 	r.V1NonAuth.GET(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.GET, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.GET, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1NonAuthPost(path string, handler func(c context.Ctx)) {
 	r.V1NonAuth.POST(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.POST, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.POST, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1NonAuthPut(path string, handler func(c context.Ctx)) {
 	r.V1NonAuth.PUT(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.PUT, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.PUT, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1NonAuthPatch(path string, handler func(c context.Ctx)) {
 	r.V1NonAuth.PATCH(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.PATCH, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.PATCH, path, r.requestMetrics, r.tracer))
 	})
 }
 
 func (r *Router) V1NonAuthDelete(path string, handler func(c context.Ctx)) {
 	r.V1NonAuth.DELETE(path, func(c *gin.Context) {
-		handler(context.NewContext(c, constant.DELETE, path, r.requestMetrics))
+		handler(context.NewContext(c, constant.DELETE, path, r.requestMetrics, r.tracer))
 	})
 }
