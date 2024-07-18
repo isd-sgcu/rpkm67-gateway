943743a6987806c015916939f989c2710ca507b2
Sinsuda <sinsuda.rakporpiang@gmail.com>
2024-07-18 17:10:09 +0700

    เขียนผิด

--- internal/pin/test/pin.handler_test.go
+++ internal/pin/test/pin.handler_test.go
@@ -1,12 +1,8 @@
 package test
 import (
-	//"context"
 	"testing"
-
-	//"time"
-
+	ctxMock "github.com/isd-sgcu/rpkm67-gateway/mocks/context"
 	"github.com/golang/mock/gomock"
-	//"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/isd-sgcu/rpkm67-gateway/apperror"
@@ -15,12 +11,12 @@ import (
 	mockPin "github.com/isd-sgcu/rpkm67-gateway/mocks/pin"
 	pinProto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/backend/pin/v1"
 	"go.uber.org/zap"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	validatorMock "github.com/isd-sgcu/rpkm67-gateway/mocks/validator"
+
 )
 
 
-type PinServiceTest struct {
+type PinHandlerTest struct {
  	suite.Suite
  	controller         *gomock.Controller
  	logger             *zap.Logger
@@ -37,11 +33,11 @@ type PinServiceTest struct {
 	CheckPinDtoReq    *dto.CheckPinRequest
  }
 
-func TestPinService(t *testing.T) {
+func TestPinHandler(t *testing.T) {
 	suite.Run(t, new(PinServiceTest))
 }
 
-func (t *PinServiceTest) SetupTest() {
+func (t *PinHandlerTest) SetupTest() {
 	t.controller = gomock.NewController(t.T())
  	t.logger = zap.NewNop()
 	t.pinsProto = MockPinsProto()
@@ -63,4 +59,43 @@ func (t *PinServiceTest) SetupTest() {
 	t.CheckPinProtoReq = &pinProto.CheckPinRequest{
 
 	}
+}
+
+func (t *PinHandlerTest) TestFindAllSuccess() {
+	pinSvc := mockPin.NewMockPinServiceClient(t.controller)
+	validator := validatorMock.NewMockDtoValidator(t.controller)
+	context := ctxMock.NewMockCtx(t.controller)
+	handler := pin.NewHandler(pinSvc, validator, t.logger)
+
+	expectedResp := &dto.CreateSelectionResponse{
+		Selection: t.pin,
+	}
+
+	context.EXPECT().GetString("userId").Return(t.userId)
+	groupSvc.EXPECT().FindByUserId(&dto.FindByUserIdGroupRequest{UserId: t.userId}).
+		Return(&dto.FindByUserIdGroupResponse{Group: &dto.Group{LeaderID: t.userId}}, nil)
+	context.EXPECT().Next()
+	context.EXPECT().Bind(&dto.CreateSelectionRequest{}).SetArg(0, *t.CreateSelectionReq)
+	validator.EXPECT().Validate(t.CreateSelectionReq).Return(nil)
+	selectionSvc.EXPECT().Create(t.CreateSelectionReq).Return(expectedResp, nil)
+	context.EXPECT().JSON(http.StatusCreated, expectedResp)
+
+	handler.Create(context)
+
+	client := mockPin.NewMockPinServiceClient(t.controller)
+	svc := pin.NewService(client, t.logger)
+	
+	protoResp := &pinProto.FindAllPinResponse{
+		Pins: t.pinsProto,
+	}
+	expected := &dto.FindAllPinResponse{
+		Pins: t.pinsDto,
+	}
+	
+	client.EXPECT().FindAll(gomock.Any(), t.FindAllPinProtoReq).Return(protoResp, nil)
+	
+	actual, err := svc.FindAll(t.FindAllPinDtoReq)
+	
+	t.Nil(err)
+	t.Equal(expected, actual)
 }
